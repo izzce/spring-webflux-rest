e3de9da24e838ff623c5942dea0cce9eebbd264f
izzetc <izzet.celik@nokia.com>
2022-10-25 23:49:49 +0300

    initial commit

---
+++ src/main/java/org/izce/spring_webflux_rest/SpringWebfluxRestApp.java
@@ -0,0 +1,13 @@
+package org.izce.spring_webflux_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxRestApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxRestApp.class, args);
+	}
+
+}
---
+++ src/test/java/org/izce/spring_webflux_rest/SpringWebfluxRestAppTests.java
@@ -0,0 +1,13 @@
+package org.izce.spring_webflux_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxRestAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
