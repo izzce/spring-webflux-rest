b92aa10179d55c02174b38a940b88d03271bf138
izzetc <izzet.celik@nokia.com>
2023-01-02 22:24:01 +0300

    Update to de.flapdoodle.embed.mongo.spring27x. Closes #1

--- src/test/java/org/izce/spring_webflux_rest/SpringWebfluxRestAppTests.java
+++ src/test/java/org/izce/spring_webflux_rest/SpringWebfluxRestAppTests.java
@@ -1,9 +1,15 @@
 package org.izce.spring_webflux_rest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SpringWebfluxRestApp.class)
+//@DataMongoTest(
+//		  excludeAutoConfiguration = org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration.class
+//		)
 class SpringWebfluxRestAppTests {
 
 	@Test
